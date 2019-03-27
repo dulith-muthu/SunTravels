e1c103d309b27d20dabf1bf100d33f36969adb9d
dulithm <dulithm@codegen.net>
2019-03-27 09:57:41 +0530

    Style login, style file structure changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './modules/login/login.component';
 
 const routes: Routes = [
-  { path: "", redirectTo: '/hotels', pathMatch: 'full' },
+  { path: "", redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'hotels', loadChildren: "./modules/hotel/hotel.module#HotelModule" },
   { path: 'contracts', loadChildren: "./modules/contract/contract.module#ContractModule" },
   { path: 'search', loadChildren: "./modules/search/search.module#SearchModule" },
